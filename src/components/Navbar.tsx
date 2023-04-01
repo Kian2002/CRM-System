b944f4b38501234ca088a6298d86c2849b32f4e0
Kian2002 <canadakurdistan@gmail.com>
2023-04-01 01:56:01 -0700

    make navbar responsive

--- src/components/Navbar.tsx
+++ src/components/Navbar.tsx
@@ -2,16 +2,16 @@ import React from "react";
 import Link from "next/link";
 
 const Navbar = () => {
-  const isLoggedIn = true; // TODO: Replace with actual logic
+  const isLoggedIn = false; // TODO: Replace with actual logic
 
   return (
-    <header className="flex w-full justify-start items-center bg-slate-800 text-white px-6 py-4">
+    <header className="flex w-full justify-between items-center bg-slate-800 text-white p-3 sm:px-6 sm:py-4">
       <h1 className="text-3xl border-b-2 hover:text-slate-400 hover:border-slate-400">
         <Link href="/">Next CRM</Link>
       </h1>
 
-      <nav className="ml-6 w-9/12 text-md text-slate-400 flex justify-start items-center">
-        <ul className="flex gap-6 hover:[&>*]:text-white">
+      <nav className="ml-6 sm:text-lg text-slate-400 text-xs">
+        <ul className="flex gap-6 hover:[&>*]:text-white hover:[&>*]:border-b">
           {isLoggedIn ? (
             <>
               <li>
