78cde55220197edae8be1574cdf516bc4fe66ecf
Kian2002 <canadakurdistan@gmail.com>
2023-03-31 20:49:48 -0700

    create navbar

--- src/components/Navbar.tsx
+++ src/components/Navbar.tsx
@@ -1,7 +1,42 @@
 import React from "react";
+import Link from "next/link";
 
 const Navbar = () => {
-  return <div>Navbar</div>;
+  const isLoggedIn = true; // TODO: Replace with actual logic
+
+  return (
+    <header className="flex w-full justify-start items-center bg-slate-800 text-white px-6 py-4">
+      <h1 className="text-3xl border-b-2 hover:text-slate-400 hover:border-slate-400">
+        <Link href="/">Next CRM</Link>
+      </h1>
+
+      <nav className="ml-6 w-9/12 text-md text-slate-400 flex justify-start items-center">
+        <ul className="flex gap-6 hover:[&>*]:text-white">
+          {isLoggedIn ? (
+            <>
+              <li>
+                <Link href="/new-record">Add Record</Link>
+              </li>
+              <li>
+                <Link href="/logout">Logout</Link>
+              </li>
+            </>
+          ) : (
+            <>
+              <li>
+                <Link href="/login">Login</Link>
+              </li>
+              <li>
+                <Link href="/register">Register</Link>
+              </li>
+            </>
+          )}
+        </ul>
+      </nav>
+    </header>
+  );
 };
 
 export default Navbar;
+
+// TODO: use next serverless functions to handle login and register
