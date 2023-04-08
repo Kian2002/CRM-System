5d8f8f668de9d2f6dc34300d9ddb17f605e69730
Kian2002 <canadakurdistan@gmail.com>
2023-04-08 15:36:29 -0700

    switch generic 'message' to error and success

--- src/pages/api/register.ts
+++ src/pages/api/register.ts
@@ -9,14 +9,14 @@ export default async function handler(
   res: NextApiResponse
 ) {
   if (req.method !== "POST") {
-    res.status(405).json({ message: "Method not allowed" });
+    res.status(405).json({ error: "Method not allowed" });
     return;
   }
 
   const { email, password } = req.body;
 
   if (!email || !password) {
-    res.status(400).json({ message: "Email and password are required" });
+    res.status(400).json({ error: "Email and password are required" });
     return;
   }
 
@@ -25,7 +25,7 @@ export default async function handler(
   );
 
   if (!regexp.test(email)) {
-    res.status(400).json({ message: "Invalid email" });
+    res.status(400).json({ error: "Invalid email" });
     return;
   }
 
@@ -36,7 +36,7 @@ export default async function handler(
   });
 
   if (userExists) {
-    res.status(400).json({ message: "User already exists" });
+    res.status(400).json({ error: "User already exists" });
     return;
   }
 
@@ -50,7 +50,7 @@ export default async function handler(
   });
 
   res.status(200).json({
-    message: "User created",
+    success: "User created",
     user: {
       id: user.id,
       email: user.email,
